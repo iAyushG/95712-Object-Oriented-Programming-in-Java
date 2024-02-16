3d2a22ba9bf8f36aa6a5263870c6f30586d0848c
Ayush Gupta <66333673+iAyushG@users.noreply.github.com>
2024-02-15 23:25:22 -0500

    Delete Exam/exam2 directory

--- Exam/exam2/Encryptable.java
+++
@@ -1,7 +0,0 @@
-package exam2;
-
-public interface Encryptable {
-	static final char SPACE = 32;  //ASCII code for space
-	public String encrypt(); //Takes message and key of the message-instance that calls it and returns and encrypted string 
-	public String decrypt(); //Takes message and key of the message-instance that calls it and returns a decrypted string
-}
